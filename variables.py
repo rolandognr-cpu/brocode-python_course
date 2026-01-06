f73569a3b96f3a190171ee521a93ca4069212359
Rolando Rodriguez <rolandognr@gmail.com>
2026-01-06 14:03:23 -0600

    fourth commit

---
+++ variables.py
@@ -0,0 +1,55 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+
+#Strings
+first_name = "Rolando"
+food = "steak"
+email = "rolando@usinternet.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is: {email}")
+
+
+#Integers
+age = 39
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+#Floats
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is: ${price}")
+print(f"your gpa is: {gpa}")
+print(f"You ran {distance} miles")
+
+#booleans
+is_student = True
+for_sale = False
+is_online = False
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
+
+
+if for_sale:
+    print("That item is for sale")
+else:
+    print("That item is not available")
+
+if is_online:
+    print("That item is online")
+else:
+    print("That item is offline")
+
+
+
